02a72177e05a2d5210794146f5200314fac4c4fb
wangwenwen <wangwenwen@51Talk.com>
2018-08-03 16:17:02 +0800

    第一次提交

---
+++ FYBannerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FYBannerView
+//
+//  Created by wang on 2018/8/3.
+//  Copyright © 2018年 wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FYBannerViewTests/FYBannerViewTests.m
@@ -0,0 +1,39 @@
+//
+//  FYBannerViewTests.m
+//  FYBannerViewTests
+//
+//  Created by wang on 2018/8/3.
+//  Copyright © 2018年 wang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FYBannerViewTests : XCTestCase
+
+@end
+
+@implementation FYBannerViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
